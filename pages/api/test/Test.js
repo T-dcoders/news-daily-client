18060f3885b87ae3e8a717d2dedbc477da03f028
T-dcoders <dcoders00@gmail.com>
2022-07-18 16:42:29 +0600

    test push after change username and pass

--- pages/api/test/Test.js
+++ pages/api/test/Test.js
@@ -4,6 +4,8 @@ const Test = () => {
     return (
         <div>
             <h1>nothing </h1>
+            <p>I am from d-coders</p>
+            <p>after change username and password </p>
         </div>
     );
 };
