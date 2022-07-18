ad62fb0e55d4a6aa9738605ee472bd132b742c91
Mahmudul <work.mahmudulhasan@gmail.com>
2022-07-18 16:56:35 +0600

    after clone to check the username

--- pages/api/test/Test.js
+++ pages/api/test/Test.js
@@ -7,6 +7,7 @@ const Test = () => {
             <p>I am from d-coders</p>
             <p>after change username and password </p>
             <p>this is after add development branch</p>
+            <p>this is after clone the repo as a developer </p>
         </div>
     );
 };
