6e5ae2e577c8e93d6d4a2ef60b604fc04e7c310a
Mahmudul <work.mahmudulhasan@gmail.com>
2022-07-18 17:22:03 +0600

    update navbar for test pourpus by mahmudul

---
+++ pages/api/test/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <p>this is the test nav bar developed by mahmudul</p>
+        </div>
+    );
+};
+
+export default Navbar;
