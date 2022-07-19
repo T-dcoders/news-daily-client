8720e4b37b6f73f0a7a357a3d54b257a2640705b
Mahadeb Sen <mahadebsen237@gmail.com>
2022-07-19 23:31:16 +0530

    mahadeb sen

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="mx-auto">
+      <p className=" text-center">Hellow </p>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
