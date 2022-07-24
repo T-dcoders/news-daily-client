20686794f8a5c69bd9507bf34c55f6902af250c9
Mizanul Islam Panna <mizanulislampanna>
2022-07-24 23:54:30 +0600

    gallery added

---
+++ pages/homepagegallery.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const homepagegallery = () => {
+  return (
+    <div>
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2 mt-5">
+            <a href="#slide4" className="btn btn-circle">
+              ❮
+            </a>
+
+            <div className="w-[95%]  bg-gray-700">
+              <p className="text-white">Date</p>
+            </div>
+
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide4" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img src="https://placeimg.com/800/200/arch" className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default homepagegallery;
